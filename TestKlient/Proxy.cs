b31af767bfa24dc2ee750bd23bb34d6f8adc9056
Kristian Enge <kristian.ienge@gmail.com>
2016-03-01 08:39:16 +0100

    enfore gitignore

---
+++ TestKlient/Proxy.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Security;
+using System.ServiceModel.Security.Tokens;
+using System.Text;
+using System.Xml;
+using OppslagstjenesteWsdl.OppslagstjenesteV5;
+
+namespace TestKlient
+{
+    public static class Proxy
+    {
+        public static oppslagstjeneste1602Client GetClient()
+        {
+            
+            ServicePointManager.ServerCertificateValidationCallback += EasyCertCheck;
+            
+            var dnsIdentity = EndpointIdentity.CreateDnsIdentity("DIREKTORATET FOR FORVALTNING OG IKT");
+
+            var address = new EndpointAddress(new Uri("https://kontaktinfo-ws-ver2.difi.no/kontaktinfo-external/ws-v5"),
+                dnsIdentity);
+
+            var myCustomBinding = CreateCustomBinding();
+            var client = new oppslagstjeneste1602Client(myCustomBinding, address);
+
+            client.ClientCredentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser,
+                StoreName.My,
+                X509FindType.FindByThumbprint,
+                "8702F5E55217EC88CF2CCBADAC290BB4312594AC");
+            client.ClientCredentials.ServiceCertificate.SetDefaultCertificate(StoreLocation.CurrentUser,
+                StoreName.TrustedPeople,
+                X509FindType.FindByThumbprint,
+                "a4 7d 57 ea f6 9b 62 77 10 fa 0d 06 ec 77 50 0b af 71 c4 32");
+            var protectionlevel = client.ChannelFactory.Endpoint.Contract.ProtectionLevel;
+            
+            client.ChannelFactory.Endpoint.Contract.ProtectionLevel = ProtectionLevel.Sign;
+            return client;
+        }
+
+        
+        private static
+            bool EasyCertCheck(object sender, X509Certificate cert,
+            X509Chain chain, SslPolicyErrors error)
+        {
+            return true;
+        }
+
+        private static CustomBinding CreateCustomBinding()
+        {
+            
+            var asbe =
+                   SecurityBindingElement.CreateMutualCertificateDuplexBindingElement();
+            asbe.MessageSecurityVersion = MessageSecurityVersion
+                .WSSecurity10WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10;
+            asbe.DefaultAlgorithmSuite = SecurityAlgorithmSuite.Basic256Sha256;
+            asbe.IncludeTimestamp = true;
+            asbe.KeyEntropyMode = SecurityKeyEntropyMode.CombinedEntropy;
+            asbe.SecurityHeaderLayout = SecurityHeaderLayout.Lax;
+            asbe.RequireSignatureConfirmation = false;
+            asbe.MessageProtectionOrder = MessageProtectionOrder.SignBeforeEncrypt;
+            
+            
+
+            var securityElementMutualBinding =
+                SecurityBindingElement.CreateMutualCertificateBindingElement(
+                    MessageSecurityVersion
+                        .WSSecurity10WSTrustFebruary2005WSSecureConversationFebruary2005WSSecurityPolicy11BasicSecurityProfile10);
+            securityElementMutualBinding.DefaultAlgorithmSuite = SecurityAlgorithmSuite.Basic256Sha256;
+            ((AsymmetricSecurityBindingElement) securityElementMutualBinding).AllowSerializedSigningTokenOnReply = true;
+            ((AsymmetricSecurityBindingElement) securityElementMutualBinding).MessageProtectionOrder = MessageProtectionOrder.SignBeforeEncrypt;
+            
+
+            var tmee = new TextMessageEncodingBindingElement();
+            tmee.MaxReadPoolSize = 64;
+            tmee.MaxWritePoolSize = 16;
+            tmee.MessageVersion = MessageVersion.Soap12;
+            tmee.WriteEncoding = Encoding.UTF8;
+            tmee.ReaderQuotas.MaxDepth = 32;
+            tmee.ReaderQuotas.MaxStringContentLength = 8192;
+            tmee.ReaderQuotas.MaxArrayLength = 16384;
+            tmee.ReaderQuotas.MaxBytesPerRead = 4096;
+            tmee.ReaderQuotas.MaxNameTableCharCount = 16384;
+
+            var tmebe = new MyMessageEncodingBindingElement();
+            tmebe.MessageVersion = MessageVersion.Soap12;
+            
+            
+            var htbe = new HttpsTransportBindingElement();
+            htbe.RequireClientCertificate = true;
+            htbe.MaxBufferPoolSize = 524288;
+            htbe.MaxBufferSize =2147483647;
+            htbe.MaxReceivedMessageSize = 2147483647;
+
+            var binding = new CustomBinding();
+            binding.Elements.Add(securityElementMutualBinding);
+            binding.Elements.Add(tmebe);
+            binding.Elements.Add(htbe);
+            return new CustomBinding(binding);
+        }
+    }
+}
---
+++ TestKlient/Responsevalidator.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+using Difi.Felles.Utility.Security;
+
+
+namespace TestKlient
+{
+    public abstract class Responsvalidator
+    {
+        protected XmlNamespaceManager Nsmgr;
+
+        public XmlDocument MottattDokument { get; private set; }
+
+        public XmlDocument SendtDokument { get; private set; }
+
+        protected XmlElement HeaderSecurityElement { get; private set; }
+
+        protected XmlElement HeaderSignatureElement { get; private set; }
+
+        protected XmlElement HeaderSignature { get; private set; }
+
+        protected Responsvalidator(XmlDocument sendtDokument, XmlDocument mottattDokument, SoapVersion version, X509Certificate2 xmlDekrypteringsSertifikat = null)
+        {
+            SendtDokument = sendtDokument;
+            MottattDokument = mottattDokument;
+
+            Nsmgr = new XmlNamespaceManager(MottattDokument.NameTable);
+            Nsmgr.AddNamespace("env", version == SoapVersion.Soap11 ? Navnerom.SoapEnvelope : Navnerom.SoapEnvelopeEnv12);
+            Nsmgr.AddNamespace("wsse", Navnerom.WssecuritySecext10);
+            Nsmgr.AddNamespace("ds", Navnerom.XmlDsig);
+            Nsmgr.AddNamespace("xenc", Navnerom.xenc);
+            Nsmgr.AddNamespace("wsse11", Navnerom.WssecuritySecext11);
+            Nsmgr.AddNamespace("wsu", Navnerom.WssecurityUtility10);
+
+            HeaderSecurityElement = MottattDokument.SelectSingleNode("/env:Envelope/env:Header/wsse:Security", Nsmgr) as XmlElement;
+            HeaderSignatureElement = HeaderSecurityElement.SelectSingleNode("./ds:Signature", Nsmgr) as XmlElement;
+            HeaderSignature = HeaderSignatureElement.SelectSingleNode("./ds:SignatureValue", Nsmgr) as XmlElement;
+
+            if (xmlDekrypteringsSertifikat != null)
+                DecryptDocument(xmlDekrypteringsSertifikat);
+        }
+
+        public XmlElement HeaderBinarySecurityToken { get; set; }
+
+
+        private void DecryptDocument(X509Certificate2 decryptionSertificate)
+        {
+            var encryptedNode = MottattDokument.SelectSingleNode("/env:Envelope/env:Body/xenc:EncryptedData", Nsmgr) as XmlElement;
+            if (encryptedNode == null)
+                return;
+
+            var encryptedXml = new EncryptedXml(MottattDokument);
+            var encryptedData = new EncryptedData();
+            encryptedData.LoadXml(encryptedNode);
+
+            var privateKey = decryptionSertificate.PrivateKey as RSACryptoServiceProvider;
+            var cipher = MottattDokument.SelectSingleNode("/env:Envelope/env:Header/wsse:Security/xenc:EncryptedKey/xenc:CipherData/xenc:CipherValue", Nsmgr).InnerText;
+
+            AesManaged aes = new AesManaged
+            {
+                Mode = CipherMode.CBC,
+                KeySize = 256,
+                Padding = PaddingMode.None,
+                Key = privateKey.Decrypt(Convert.FromBase64String(cipher), true)
+            };
+
+            encryptedXml.ReplaceData(encryptedNode, encryptedXml.DecryptData(encryptedData, aes));
+        }
+
+
+        protected virtual void SjekkTimestamp(TimeSpan timeSpan)
+        {
+            var timestampElement = HeaderSecurityElement.SelectSingleNode("./wsu:Timestamp", Nsmgr);
+
+            var created = DateTimeOffset.Parse(timestampElement["Created", Navnerom.WssecurityUtility10].InnerText);
+            var expires = DateTimeOffset.Parse(timestampElement["Expires", Navnerom.WssecurityUtility10].InnerText);
+
+            if (created > DateTimeOffset.Now.AddMinutes(5))
+                throw new Exception("Motatt melding har opprettelsetidspunkt mer enn 5 minutter inn i fremtiden." + created.ToString());
+            if (created < DateTimeOffset.Now.Add(timeSpan.Negate()))
+                throw new Exception(string.Format("Motatt melding har opprettelsetidspunkt som er eldre enn {0} minutter.", timeSpan.Minutes));
+
+            if (expires < DateTimeOffset.Now)
+                throw new Exception("Motatt melding har utgått på tid.");
+        }
+
+        /// <summary>
+        /// Sjekker at soap envelopen inneholder timestamp, body og messaging element med korrekt id og referanser i security signaturen.
+        /// </summary>
+        protected void ValiderSignaturreferanser(XmlElement signature, SignedXmlWithAgnosticId signedXml, string[] påkrevdeReferanser)
+        {
+            foreach (var påkrevdReferanse in påkrevdeReferanser)
+            {
+                var node = InneholderNode(påkrevdReferanse);
+                var elementId = NodeFinnesISignaturElement(signature, node, påkrevdReferanse);
+
+                IdNodeMatcher(signedXml, elementId, node, påkrevdReferanse);
+            }
+        }
+
+        private void IdNodeMatcher(SignedXmlWithAgnosticId signedXml, string elementId, XmlNodeList nodes, string elementXPath)
+        {
+            var targetNode = signedXml.GetIdElement(MottattDokument, elementId);
+            if (targetNode != nodes[0])
+                throw new Exception(string.Format("Signaturreferansen med id '{0}' må refererer til node med sti '{1}'",
+                    elementId, elementXPath));
+        }
+
+        private string NodeFinnesISignaturElement(XmlElement signature, XmlNodeList nodes, string elementXPath)
+        {
+            var elementId = nodes[0].Attributes["wsu:Id"].Value;
+
+            var references = signature.SelectNodes(string.Format("./ds:SignedInfo/ds:Reference[@URI='#{0}']", elementId), Nsmgr);
+            if (references == null || references.Count == 0)
+            {
+                throw new Exception(
+                    string.Format("Kan ikke finne påkrevet refereanse til element '{0}' i signatur fra meldingsformidler.", elementXPath)
+                    );
+            }
+            if (references.Count > 1)
+                throw new Exception(
+                    string.Format("Påkrevet refereanse til element '{0}' kan kun forekomme én gang i signatur fra meldingsformidler. Ble funnet {1} ganger.", elementXPath, references.Count)
+                    );
+            return elementId;
+        }
+
+        private XmlNodeList InneholderNode(string elementXPath)
+        {
+            var nodes = MottattDokument.SelectNodes(elementXPath, Nsmgr);
+            if (nodes == null || nodes.Count == 0)
+            {
+                throw new Exception(string.Format("Kan ikke finne påkrevet element '{0}' i svar fra meldingsformidler.", elementXPath));
+            }
+
+            if (nodes.Count > 1)
+            {
+                throw new Exception(
+                    string.Format("Påkrevet element '{0}' kan kun forekomme én gang i svar fra meldingsformidler. Ble funnet {1} ganger.", elementXPath, nodes.Count)
+                    );
+            }
+
+            return nodes;
+        }
+
+
+        protected void PerformSignatureConfirmation(XmlElement securityNode)
+        {
+            // Locate SignatureConfirmation element in response document
+            var signatureConfirmationNode = securityNode.SelectSingleNode("./wsse11:SignatureConfirmation", Nsmgr);
+            var signatureConfirmation = signatureConfirmationNode.Attributes["Value"].Value;
+
+            // Locate sent signature
+            var sentSignatureValueNode = SendtDokument.SelectSingleNode("/env:Envelope/env:Header/wsse:Security/ds:Signature/ds:SignatureValue", Nsmgr);
+            var sentSignatureValue = sentSignatureValueNode.InnerText;
+
+            // match against sent signature
+            if (signatureConfirmation != sentSignatureValue)
+                throw new Exception(string.Format("Motatt signaturbekreftelse '{0}' er ikke lik sendt signatur '{1}'.", signatureConfirmation, sentSignatureValue));
+
+        }
+    }
+
+    
+        public enum SoapVersion
+        {
+            Soap11,
+            Soap12
+        }
+    
+}
